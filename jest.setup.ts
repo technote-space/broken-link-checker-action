0a82a2cb7a13ff33a3d77967e34530f1643dc51c
Technote <technote.space@gmail.com>
2020-06-23 22:35:22 +0900

    fix: broken test

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,7 +2,6 @@ import {setupGlobal} from '@technote-space/github-action-test-helper';
 
 setupGlobal();
 
-jest.mock('./src/constant', () => ({
-  ...jest.requireActual('./src/constant'),
+jest.mock('./src/constant', () => Object.assign(jest.requireActual('./src/constant'), {
   INTERVAL_MS: 0,
 }));
